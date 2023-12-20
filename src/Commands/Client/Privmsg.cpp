d3d6526b9c777fcaa82c819ea2c97a9c3fb656e0
vimercie <vimercie@student.42lyon.fr>
2023-12-20 19:22:03 +0100

    Refactor privmsg function and its related methods

--- src/Commands/Client/Privmsg.cpp
+++ src/Commands/Client/Privmsg.cpp
@@ -6,7 +6,7 @@
 /*   By: vimercie <vimercie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 17:24:06 by vimercie          #+#    #+#             */
-/*   Updated: 2023/12/20 18:01:29 by vimercie         ###   ########lyon.fr   */
+/*   Updated: 2023/12/20 18:38:16 by vimercie         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,47 @@
 #include "../../../inc/Channel.hpp"
 #include "../../../inc/IRCmsg.hpp"
 #include "../../../inc/RPL.hpp"
+
+int privmsgToClient(const IRCmsg& msg, Client* recipient)
+{
+	const Client* sender = msg.getClient();
+
+	IRCmsg response;
+	response.setClient(recipient);
+	response.setPrefix(user_id(sender->getNickname(), sender->getUsername()));
+	response.setCommand("PRIVMSG");
+	response.setParameters(std::vector<std::string>(1, recipient->getNickname()));
+	response.setTrailing(msg.getTrailing());
+
+	recipient->appendToSendBuffer(response.toString());
+
+	return 0;
+}
+
+int privmsgToChannel(const IRCmsg& msg, Channel* channel)
+{
+	std::vector<Client*>	clients = channel->getClients();
+	Client* 				sender = msg.getClient();
+
+	if (std::find(clients.begin(), clients.end(), sender) == clients.end())
+	{
+		std::cout << "Client " << sender->getNickname() << " not in channel " << channel->getName() << std::endl;
+		sender->appendToSendBuffer(ERR_NOTONCHANNEL(channel->getName()));
+		return 0;
+	}
+
+   	for (std::vector<Client*>::const_iterator it = clients.begin(); it != clients.end(); it++)
+	{
+		Client* client = *it;
+
+		if (!client || client == sender)
+			continue;
+
+		client->appendToSendBuffer(msg.toString());
+	}
+
+	return 0;
+}
 
 int Server::privmsg(const IRCmsg& msg)
 {
@@ -44,45 +85,4 @@ int Server::privmsg(const IRCmsg& msg)
 	response.setPrefix(user_id(sender->getNickname(), sender->getUsername()));
 
 	return privmsgToChannel(response, channel);
-}
-
-int Server::privmsgToClient(const IRCmsg& msg, Client* recipient)
-{
-	const Client* sender = msg.getClient();
-
-	IRCmsg response;
-	response.setClient(recipient);
-	response.setPrefix(user_id(sender->getNickname(), sender->getUsername()));
-	response.setCommand("PRIVMSG");
-	response.setParameters(std::vector<std::string>(1, recipient->getNickname()));
-	response.setTrailing(msg.getTrailing());
-
-	recipient->appendToSendBuffer(response.toString());
-
-	return 0;
-}
-
-int Server::privmsgToChannel(const IRCmsg& msg, Channel* channel)
-{
-	std::vector<Client*>	clients = channel->getClients();
-	Client* 				sender = msg.getClient();
-
-	if (std::find(clients.begin(), clients.end(), sender) == clients.end())
-	{
-		std::cout << "Client " << sender->getNickname() << " not in channel " << channel->getName() << std::endl;
-		sender->appendToSendBuffer(ERR_NOTONCHANNEL(channel->getName()));
-		return 0;
-	}
-
-   	for (std::vector<Client*>::const_iterator it = clients.begin(); it != clients.end(); it++)
-	{
-		Client* client = *it;
-
-		if (!client || client == sender)
-			continue;
-
-		client->appendToSendBuffer(msg.toString());
-	}
-
-	return 0;
 }
